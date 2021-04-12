d21daf008f7b9cff978b7d74eda8da2f3e389360
monivaanan <60361425+monivaanan@users.noreply.github.com>
2021-04-12 15:54:03 +0530

    Add files via upload

---
+++ ACreateLead.java
@@ -0,0 +1,40 @@
+package AssignmentWeek5;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ACreateLead {
+	
+	@Test(invocationCount =3)
+
+	public void executeACreateLead() {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver= new ChromeDriver();
+		driver .manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Zoho");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Monisha");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Ayesha");
+		driver.findElement(By.name("submitButton")).click();
+		driver.close();
+}
+}
+
+
+
+
+
+
